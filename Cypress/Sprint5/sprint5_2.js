b37ca8aa07df87c0bf1fe326019beab3b6bfc136
irfhan10 <48820448+irfhan10@users.noreply.github.com>
2021-08-31 22:24:14 +0700

    sudah di perbaiki

--- Cypress/Sprint5/sprint5_2.js
+++ Cypress/Sprint5/sprint5_2.js
@@ -7,6 +7,7 @@ describe('upload file', function() {
     //     cy.get('.d-flex > .btn').click()
     //     cy.get('input[type="file"]').attachFile('admin.png')
     // }),
+
     it('Tugas 5 Sprint 5', function() {
         cy.visit('https://demoqa.com/automation-practice-form')
         cy.get('#firstName').type('Zilong')
@@ -19,10 +20,12 @@ describe('upload file', function() {
             .should('have.text', 'Male')
         cy.get('#userNumber').type('1234567890')
             .should('have.value', '1234567890')
-        cy.get('#dateOfBirthInput')
-            .should('have.value', '31 Aug 2021')
-        cy.get('.subjects-auto-complete__value-container').type('Cypress{enter}')
-          //  .should('have.text', 'Cypress')
+        cy.get('#dateOfBirthInput', {force: true})
+            .click().type('{selectall}','{backspace}').type('12 Dec 2000').type('{enter}')
+            .should('have.value', '12 Dec 2000')
+        cy.get('.subjects-auto-complete__value-container').type('Economics{enter}')
+        cy.get('.subjects-auto-complete__value-container').type('MATHS{enter}')
+        cy.get('.subjects-auto-complete__value-container').type('English{enter}')
         cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(1)').click()
             .should('have.text', 'Sports')
         cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(3)').click()
@@ -30,15 +33,16 @@ describe('upload file', function() {
         cy.get('#uploadPicture').attachFile('admin.png')
         cy.get('#currentAddress').type('Tangerang')
             .should('have.value', 'Tangerang')
-        cy.get('#state > .css-yk16xz-control > .css-1hwfws3 > .css-1wa3eu0-placeholder').select('NCR')
-          //  .should('be.selected')
-        //cy.get('#stateCity-wrapper')
-          //   .should('not.be.visible', {force: true})
-        // cy.get('#submit').click()
-        // cy.get('.modal-header').should('have.text', 'Thanks for submitting the form')
+        cy.get('#state > .css-yk16xz-control > .css-1hwfws3 > .css-1wa3eu0-placeholder').click()
+        cy.get('#react-select-3-option-2').click()
+        cy.get('#city > .css-yk16xz-control > .css-1hwfws3').click()
+        cy.get('#react-select-4-option-0').click()
+        cy.get('#submit').click()
+        cy.get('.modal-header')
+            .should('have.text', 'Thanks for submitting the form')
     })
 
-    })
+})
     Cypress.on('uncaught:exception', (err, runnable)=>{
         return false
 })
